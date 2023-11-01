d21956aa07e04eea251796739bce08e9d71f7533
41018160 <148927549+41018160@users.noreply.github.com>
2023-11-01 10:23:32 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,85 @@
+<h2 align="left">顯示 student 資料表所有資料</h2>
+<table border="1" cellpadding="0" cellspacing="0">
+    <th>編號</th><th>姓名</th><th>性別</th><th>生日</th>
+    <th>郵件帳號</th><th>電話</th><th>地址</th>
+        <tr>
+            <td>1 </td>
+            <td>李采茜 </td>
+            <td>F  </td>
+            <td>1987年4月4日 </td>
+            <td>elven@superstar.com </td>
+            <td>0976123456</td>
+            <td>台北市信義路643號 </td>    
+        </tr>
+	<tr>
+            <td>2 </td>
+            <td>林木田 </td>
+            <td>M  </td>
+            <td>2001年8月9日 </td>
+            <td>david@superstar.com </td>
+            <td>0978125643</td>
+            <td>台北市仁愛路43號 </td>    
+        </tr>
+	<tr>
+            <td>3 </td>
+            <td>田出火 </td>
+            <td>M  </td>
+            <td>1998年12月30日 </td>
+            <td>lily@superstar.com </td>
+            <td>0945678912</td>
+            <td>台北市忠孝東路12號 </td>    
+        </tr>
+	<tr>
+            <td>4 </td>
+            <td>張美麗 </td>
+            <td>F  </td>
+            <td>2008年6月24日 </td>
+            <td>beauty@superstar.com </td>
+            <td>0912563478</td>
+            <td>台北市信義路98號 </td>    
+        </tr>
+	<tr>
+            <td>5 </td>
+            <td>鄭麗珠 </td>
+            <td>F  </td>
+            <td>1980年8月1日 </td>
+            <td>joe@superstar.com </td>
+            <td>0956784321</td>
+            <td>台北市和平東路56號 </td>    
+        </tr>
+	<tr>
+            <td>6 </td>
+            <td>黃竹林 </td>
+            <td>M  </td>
+            <td>1994年1月29日 </td>
+            <td>john@superstar.com </td>
+            <td>0966665555</td>
+            <td>台北市南昌路65號 </td>    
+        </tr>
+	<tr>
+            <td>7 </td>
+            <td>邱諒金 </td>
+            <td>M  </td>
+            <td>2005年4月23日 </td>
+            <td>goan@superstar.com </td>
+            <td>0913427777</td>
+            <td>台北市信義路1008 </td>    
+        </tr>
+	<tr>
+            <td>8 </td>
+            <td>黃珠文 </td>
+            <td>F  </td>
+            <td>1992年4月25日 </td>
+            <td>mary@superstar.com </td>
+            <td>0933338888</td>
+            <td>台北市南海路13號 </td>    
+        </tr>
+	<tr>
+            <td>9 </td>
+            <td>顏伯丞 </td>
+            <td>M  </td>
+            <td>2003年8月4日 </td>
+            <td>41018160@gcloud.csu.edu.tw </td>
+            <td>0912345678</td>
+            <td>台北市南昌路65號 </td>    
+        </tr>
